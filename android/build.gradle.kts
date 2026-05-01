7dbe4a856f9677e52e957ce2bd709d3b013190dd
Thamu <movieinsight908@gmail.com>
2026-05-01 14:34:29 +0530

    Use Kotlin compiler options DSL

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -28,8 +28,8 @@ subprojects {
     }
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_17.toString()
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
     
