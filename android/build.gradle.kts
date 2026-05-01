4d3dbc08d3f667f8d40eab2dfae31679921a67ac
Thamu <movieinsight908@gmail.com>
2026-05-01 14:42:21 +0530

    Force Android plugin Java target to 17

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -38,6 +38,19 @@ subprojects {
             val androidExt = project.extensions.findByName("android")
             if (androidExt != null) {
                 try {
+                    val compileOptions = androidExt.javaClass.methods
+                        .find { it.name == "getCompileOptions" }
+                        ?.invoke(androidExt)
+
+                    if (compileOptions != null) {
+                        compileOptions.javaClass.methods
+                            .find { it.name == "setSourceCompatibility" && it.parameterTypes.size == 1 }
+                            ?.invoke(compileOptions, JavaVersion.VERSION_17)
+                        compileOptions.javaClass.methods
+                            .find { it.name == "setTargetCompatibility" && it.parameterTypes.size == 1 }
+                            ?.invoke(compileOptions, JavaVersion.VERSION_17)
+                    }
+
                     val getNamespaceMethod = androidExt.javaClass.methods.find { it.name == "getNamespace" }
                     val setNamespaceMethod = androidExt.javaClass.methods.find { it.name == "setNamespace" && it.parameterTypes.size == 1 && it.parameterTypes[0] == String::class.java }
                     
