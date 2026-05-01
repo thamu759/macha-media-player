349cb402378c3f7c710fd6cf2021eaf4d67e8362
Thamu <movieinsight908@gmail.com>
2026-05-01 21:01:42 +0530

    fix: revert mainactivity to standard state to fix build compilation

--- android/app/src/main/kotlin/com/macha/mediaplayer/macha_media_player/MainActivity.kt
+++ android/app/src/main/kotlin/com/macha/mediaplayer/macha_media_player/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.macha.mediaplayer.macha_media_player
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
-}
+class MainActivity : FlutterActivity()
