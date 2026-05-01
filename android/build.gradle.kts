579cd2f7e5db1ba4a945604e691637e59349bbdd
Thamu <movieinsight908@gmail.com>
2026-05-01 12:11:25 +0530

    Fix namespace issue for on_audio_query_android

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,17 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    afterEvaluate {
+        if (name == "on_audio_query_android") {
+            try {
+                val androidExt = extensions.getByName("android")
+                androidExt.javaClass.getMethod("setNamespace", String::class.java).invoke(androidExt, "com.lucasjosino.on_audio_query")
+            } catch (e: Exception) {
+                println("Failed to set namespace for on_audio_query_android: ${e.message}")
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
