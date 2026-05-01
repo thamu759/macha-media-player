b4294416fb638f3c3e64b7b0d136c2761b7f5fe2
Thamu <movieinsight908@gmail.com>
2026-05-01 21:23:34 +0530

    Restore Flutter Android build output path

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,7 +12,16 @@ allprojects {
     }
 }
 
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
 subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
